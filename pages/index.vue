356508fdb2a51bac8e57556651137d54dd553fbd
Samit Koyom <samitkoyom@gmail.com>
2024-07-20 01:28:17 +0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <VContainer fluid class="fill-height pa-0">
+        <VRow no-gutters align="center" justify="center" class="fill-height">
+            <VCol class="hidden-sm-and-down fill-height" md="6" lg="6">
+                <VImg
+                    src="https://wallpaper.dog/large/5557744.jpg"
+                    cover
+                    class="h-100 rounded-xl rounded-ts-0 rounded-bs-0 d-flex align-center justify-center"
+                >
+                    <div class="text-center w-50 text-white mx-auto">
+                        <h1 class="mb-4">Welcome back.</h1>
+                        <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                    </div>
+                </VImg>
+            </VCol>
+            <VCol cols="12" md="6" lg="6" sm="6">
+                
+                <VRow no-gutters align="center" justify="center">
+                    <VCol cols="12" md="6" class="pa-3">
+                        <h1>Login</h1>
+                        <p class="text-medium-emphasis">Input your email and password</p>
+                        <VForm class="mt-7">
+                            <div class="mt-1">
+                                <VTextField 
+                                    label="Email" 
+                                    variant="outlined"
+                                    prepend-inner-icon="mdi-email"
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                />
+                            </div>
+                            <div class="mt-1">
+                                <VTextField 
+                                    label="Password" 
+                                    variant="outlined"
+                                    prepend-inner-icon="mdi-lock"
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                />
+                            </div>
+                            <div class="mt-5 text-center">
+                                <VBtn type="submit" 
+                                    block
+                                    min-height="44" 
+                                    class="gradient primary"
+                                    >
+                                    Login
+                                </VBtn>
+                            </div>
+                        </VForm>
+                        <p class="text-center mt-10">
+                            <span>Don't have an account? Registe</span>&nbsp;
+                            <NuxtLink to="/register" class="text-primary">Register</NuxtLink>
+                        </p>
+                    </VCol>
+                </VRow>
+            </VCol>
+        </VRow>
+    </VContainer>
+</template>
+
+<style scoped>
+
+</style>
